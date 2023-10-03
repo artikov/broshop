2eefc2eb17d25eafc582c500244c651f17f3b6e4
Oybek Artikov <oybekartikov@icloud.com>
2023-10-03 20:25:00 +0500

    Footer & Header create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <h1>Welcom to broshop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to BroShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
