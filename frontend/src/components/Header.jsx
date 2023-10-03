7fc6b1e4834d7a9afa8f3b38464c9de04dde478a
Oybek Artikov <oybekartikov@icloud.com>
2023-10-03 21:23:13 +0500

    HomeScreen and Product component add

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,17 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 
+import logo from "../assets/2.png";
+
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">BroShop</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img width={40} src={logo} alt="logo" />
+            BroShop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
