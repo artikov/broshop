43d90d79bcfa2fe6d7cc7931530afe80659d5c4d
artikov <oybekartikov@icloud.com>
2023-10-13 20:57:10 +0500

    Update styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,10 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
