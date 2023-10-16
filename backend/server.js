1f3cba8322376b1860ca350a4c8272f51cc1e35a
artikov <oybekartikov@icloud.com>
2023-10-16 15:04:30 +0500

    prdouct router added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -14,18 +14,7 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/api/products", (req, res) => {
-  res.send(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).send({ message: "Product not found" });
-  }
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
