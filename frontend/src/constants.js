d3c8af5c4924450deb1fe3bce911e85af912730a
artikov <oybekartikov@icloud.com>
2023-10-17 10:05:32 +0500

    react-redux, redux-toolkit & apiSlice added

---
+++ frontend/src/constants.js
@@ -0,0 +1,16 @@
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
+
+// export const CART_URL = "/api/cart";
+// export const LOGIN_URL = "/api/users/login";
+// export const REGISTER_URL = "/api/users/register";
+// export const PROFILE_URL = "/api/users/profile";
+// export const SHIPPING_URL = "/api/shipping";
+// export const PAYMENT_URL = "/api/payment";
+// export const ORDER_URL = "/api/order";
+// export const ORDER_PAY_URL = "/api/order/pay";
+// export const ORDER_DELIVER_URL = "/api/order/deliver";
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
@@ -27,6 +29,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
