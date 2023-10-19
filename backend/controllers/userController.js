fac7b5cf678aedac03bcfbfa21b24921d8d28c2b
artikov <oybekartikov@icloud.com>
2023-10-19 11:18:10 +0500

    Logout user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,7 +47,8 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  POST /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
+  res.status(200).json({ message: "Logged out" });
 });
 
 // @desc   Get user profile
