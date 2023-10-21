8117603c3b222342519d56b1587e2e240e0ab883
artikov <oybekartikov@icloud.com>
2023-10-21 10:20:21 +0500

    Create & Get orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,40 @@ import Order from "../models/orderModel.js";
 // @route  POST /api/orders
 // @access Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createOrder = await order.save();
+
+    res.status(201).json(createOrder);
+  }
 });
 
 // @desc   Get logged in user orders
@@ -19,7 +52,17 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @route  GET /api/orders/:id
 // @access Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get order by id");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.staus(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc   Update order to paid
@@ -40,7 +83,8 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 // @route  GET /api/orders
 // @access Private
 const getOrders = asyncHandler(async (req, res) => {
-  res.send("get all orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 export {
