0a50e70123e1b94abf378afa3996efd6a94ed072
artikov <oybekartikov@icloud.com>
2023-10-28 12:26:30 +0500

    paginate products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,32 +3,34 @@ import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { useParams } from "react-router-dom";
 
 const HomeScreen = () => {
-  const { data: products, isLoading, isError } = useGetProductsQuery();
+	const { pageNumber } = useParams();
+	const { data, isLoading, isError } = useGetProductsQuery({ pageNumber });
 
-  return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : isError ? (
-        <Message variant={"danger"}>
-          {isError?.data?.message || isError.error}
-        </Message>
-      ) : (
-        <>
-          <h1>HomeScreen</h1>
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-        </>
-      )}
-    </>
-  );
+	return (
+		<>
+			{isLoading ? (
+				<Loader />
+			) : isError ? (
+				<Message variant={"danger"}>
+					{isError?.data?.message || isError.error}
+				</Message>
+			) : (
+				<>
+					<h1>HomeScreen</h1>
+					<Row>
+						{data.products.map((product) => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+					</Row>
+				</>
+			)}
+		</>
+	);
 };
 
 export default HomeScreen;
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -4,7 +4,10 @@ import { apiSlice } from "./apiSlice";
 export const productsApiSlice = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
 		getProducts: builder.query({
-			query: () => ({ url: PRODUCTS_URL }),
+			query: ({ pageNumber }) => ({
+				url: PRODUCTS_URL,
+				params: { pageNumber },
+			}),
 			providesTags: ["Products"],
 			keepUnusedDataFor: 5,
 		}),
