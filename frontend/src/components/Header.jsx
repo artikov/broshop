10da2db7b2bd09b1ac3062542c3a176210227470
artikov <oybekartikov@icloud.com>
2023-10-30 11:36:06 +0500

    search box component added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,6 +6,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 
+import SearchBox from "./SearchBox";
+
 import logo from "../assets/2.png";
 
 const Header = () => {
@@ -40,6 +42,7 @@ const Header = () => {
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
+							<SearchBox />
 							<LinkContainer to="/cart">
 								<Nav.Link>
 									<FaShoppingCart /> Cart
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,7 +4,7 @@ import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Paginate from "../components/Paginate";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const HomeScreen = () => {
 	const { pageNumber, keyword } = useParams();
@@ -15,6 +15,11 @@ const HomeScreen = () => {
 
 	return (
 		<>
+			{keyword && (
+				<Link to="/" className="btn btn-light mb-4">
+					Go Back
+				</Link>
+			)}
 			{isLoading ? (
 				<Loader />
 			) : isError ? (
